2258e6ae4fe8bdb67a437488f7bb3146b9a8cc6f
Lincoln Oliveira <legionario_172@hotmail.com>
2020-10-23 16:50:13 -0300

    Atualização de movimentos

--- MaquinaDeCombate.java
+++ MaquinaDeCombate.java
@@ -5,50 +5,71 @@ import java.awt.Color;
 /**
  * MaquinaDeCombate - a robot by (Lincoln Silva de Oliveira)
  */
-public class MaquinaDeCombate extends Robot
-{
-	/**
-	 * run: MaquinaDeCombate's default behavior
-	 */
+public class MaquinaDeCombate extends AdvancedRobot {
+	
+	int i = 0;
+	int fix = 10;
+	
+	
 	public void run() {
-		// Initialization of the robot should be put here
+		
+		//Definindo as cores: 
+		 setBodyColor(Color.blue);
+		 setRadarColor(Color.red);
+		 setGunColor(Color.gray);
+		 setScanColor(Color.red);
+		 setBulletColor(Color.orange);
 
-		// After trying out your robot, try uncommenting the import at the top,
-		// and the next line:
-
-		 setColors(Color.blue,Color.red,Color.gray); // body,gun,radar
-
-		// Robot main loop
 		while(true) {
-			// Replace the next 4 lines with any behavior you would like
-			ahead(100);
-			turnGunRight(360);
-			back(100);
-			turnGunRight(360);
+			
+			turnGunLeft(10);
 		}
 	}
 
-	/**
-	 * onScannedRobot: What to do when you see another robot
-	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
-		// Replace the next line with any behavior you would like
-		fire(1);
-	}
-
-	/**
-	 * onHitByBullet: What to do when you're hit by a bullet
-	 */
-	public void onHitByBullet(HitByBulletEvent e) {
-		// Replace the next line with any behavior you would like
-		back(10);
+		
+		if(i % fix == 0){
+			turnGunRight(180);
+		} else {
+			double gunTurnAmt = normalRelativeAngle(e.getBearing() + (getHeading() - getRadarHeading()));
+            setTurnGunRight(gunTurnAmt);
+            fix++;	
+            if(getDistanceRemaining() < 500 ){
+            	fire(1);
+            	ahead(10);
+            } else {
+            	ahead(20);
+            	turnRight(45);
+            }
+		}
+		
 	}
 	
-	/**
-	 * onHitWall: What to do when you hit a wall
-	 */
+	public double normalRelativeAngle(double angulo) {
+        if (angulo > -180 && angulo <= 180) {
+            return angulo;
+        }
+ 
+        double fixAngulo = angulo;
+        while (fixAngulo <= -180) {
+        	fixAngulo += 360;
+        }
+ 
+        while (fixAngulo > 180) {
+        	fixAngulo -= 360;
+        }
+ 
+        return fixAngulo;
+    }
+		
+	public void onHitByBullet(HitByBulletEvent e) {
+
+		back(20);
+		turnRight(45);
+	}
+	
 	public void onHitWall(HitWallEvent e) {
-		// Replace the next line with any behavior you would like
+		
 		back(20);
 	}	
 }
