94e2afa86441f11cd6ea512a8140b78572c40c6d
Lincoln Oliveira <legionario_172@hotmail.com>
2020-10-23 09:43:49 -0300

    Add files via upload

---
+++ MaquinaDeCombate.java
@@ -0,0 +1,54 @@
+package maquinacombate;
+import robocode.*;
+import java.awt.Color;
+
+/**
+ * MaquinaDeCombate - a robot by (Lincoln Silva de Oliveira)
+ */
+public class MaquinaDeCombate extends Robot
+{
+	/**
+	 * run: MaquinaDeCombate's default behavior
+	 */
+	public void run() {
+		// Initialization of the robot should be put here
+
+		// After trying out your robot, try uncommenting the import at the top,
+		// and the next line:
+
+		 setColors(Color.blue,Color.red,Color.gray); // body,gun,radar
+
+		// Robot main loop
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+			ahead(100);
+			turnGunRight(360);
+			back(100);
+			turnGunRight(360);
+		}
+	}
+
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		fire(1);
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+		back(10);
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		back(20);
+	}	
+}
